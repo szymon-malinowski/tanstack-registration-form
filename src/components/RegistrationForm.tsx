6ca5ec4fd7dbb5fd1b85d112a7b7e8450d37c86d
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-07-01 07:59:09 +0200

    Added Last Name

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -4,6 +4,7 @@ export default function RegistrationForm() {
   const form = useForm({
     defaultValues: {
       firstName: "",
+      lastName: "",
     },
     onSubmit: ({ value }) => {
       console.log(value);
@@ -19,6 +20,30 @@ export default function RegistrationForm() {
       }}
     >
       <h1>Seminar Registration</h1>
+      <form.Field name="firstName">
+        {(field) => (
+          <label>
+            First Name
+            <input
+              value={field.state.value}
+              onBlur={field.handleBlur}
+              onChange={(event) => field.handleChange(event.target.value)}
+            />
+          </label>
+        )}
+      </form.Field>
+      <form.Field name="lastName">
+        {(field) => (
+          <label>
+            Last Name
+            <input
+              value={field.state.value}
+              onBlur={field.handleBlur}
+              onChange={(event) => field.handleChange(event.target.value)}
+            />
+          </label>
+        )}
+      </form.Field>
       <button type="submit">Submit</button>
     </form>
   );
