7127033bfc68ac0a98694e939c6e1e5872ef1319
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-07-01 08:25:49 +0200

    Refactor RegistrationForm component layout and styles

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,4 +1,5 @@
 import { useForm } from "@tanstack/react-form";
+import "../App.css";
 
 export default function RegistrationForm() {
   const form = useForm({
@@ -12,39 +13,56 @@ export default function RegistrationForm() {
   });
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        event.stopPropagation();
-        form.handleSubmit();
-      }}
-    >
-      <h1>Seminar Registration</h1>
-      <form.Field name="firstName">
-        {(field) => (
-          <label>
-            First Name
-            <input
-              value={field.state.value}
-              onBlur={field.handleBlur}
-              onChange={(event) => field.handleChange(event.target.value)}
-            />
-          </label>
-        )}
-      </form.Field>
-      <form.Field name="lastName">
-        {(field) => (
-          <label>
-            Last Name
-            <input
-              value={field.state.value}
-              onBlur={field.handleBlur}
-              onChange={(event) => field.handleChange(event.target.value)}
-            />
-          </label>
-        )}
-      </form.Field>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="container">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <h1>Seminar Registration</h1>
+        <form.Field name="firstName">
+          {(field) => (
+            <label>
+              First Name
+              <input
+                type="text"
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(event) => field.handleChange(event.target.value)}
+              />
+            </label>
+          )}
+        </form.Field>
+        <form.Field name="lastName">
+          {(field) => (
+            <label>
+              Last Name
+              <input
+                type="text"
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(event) => field.handleChange(event.target.value)}
+              />
+            </label>
+          )}
+        </form.Field>
+        <form.Field name="firstName">
+          {(field) => (
+            <label>
+              Email
+              <input
+                type="email"
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(event) => field.handleChange(event.target.value)}
+              />
+            </label>
+          )}
+        </form.Field>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
